76e2cc7b5aa575cd51532855beefbfb372356bef
Stephen Plummer <stephen.plummer1.sp@gmail.com>
2019-10-18 01:29:55 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -2,8 +2,8 @@ class TicTacToe
   
   attr_accessor :board 
   
-  def initialize(board)
-    @board = [" ", " ", "", "" ,"" ,"", "", "", ""]
+  def initialize (board)
+    @board = ["", "" , "", "" ,"" ,"", "", "", ""]
   end
 end
   
